c22f62f09748f24d1a06c88482c214e9b048d16e
kugayamashusei <58841667+kugayamashusei@users.noreply.github.com>
2020-12-02 18:00:54 +0700

    sửa xong controller HoaDon, BinhLuan

---
+++ RapChieuPhim/RapChieuPhim/Areas/Admin/Controllers/BinhLuanController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RapChieuPhim.Areas.Admin.Data;
+using RapChieuPhim.Areas.Admin.Models;
+
+namespace RapChieuPhim.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BinhLuanController : Controller
+    {
+        private readonly DPContext _context;
+
+        public BinhLuanController(DPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/BinhLuanModels
+        public async Task<IActionResult> Index()
+        {
+            var dPContext = _context.BinhLuanModel.Include(b => b.idNguoiDung).Include(b => b.idPhim);
+            return View(await dPContext.ToListAsync());
+        }
+
+        // GET: Admin/BinhLuanModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var binhLuanModel = await _context.BinhLuanModel
+                .Include(b => b.idNguoiDung)
+                .Include(b => b.idPhim)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (binhLuanModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(binhLuanModel);
+        }
+
+        // GET: Admin/BinhLuanModels/Create
+        public IActionResult Create()
+        {
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID");
+            ViewData["Phim_ID"] = new SelectList(_context.PhimModel, "ID", "ID");
+            return View();
+        }
+
+        // POST: Admin/BinhLuanModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,NguoiDung_ID,Phim_ID,Noi_dung")] BinhLuanModel binhLuanModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(binhLuanModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID", binhLuanModel.NguoiDung_ID);
+            ViewData["Phim_ID"] = new SelectList(_context.PhimModel, "ID", "ID", binhLuanModel.Phim_ID);
+            return View(binhLuanModel);
+        }
+
+        // GET: Admin/BinhLuanModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var binhLuanModel = await _context.BinhLuanModel.FindAsync(id);
+            if (binhLuanModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID", binhLuanModel.NguoiDung_ID);
+            ViewData["Phim_ID"] = new SelectList(_context.PhimModel, "ID", "ID", binhLuanModel.Phim_ID);
+            return View(binhLuanModel);
+        }
+
+        // POST: Admin/BinhLuanModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,NguoiDung_ID,Phim_ID,Noi_dung")] BinhLuanModel binhLuanModel)
+        {
+            if (id != binhLuanModel.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(binhLuanModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BinhLuanModelExists(binhLuanModel.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID", binhLuanModel.NguoiDung_ID);
+            ViewData["Phim_ID"] = new SelectList(_context.PhimModel, "ID", "ID", binhLuanModel.Phim_ID);
+            return View(binhLuanModel);
+        }
+
+        // GET: Admin/BinhLuanModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var binhLuanModel = await _context.BinhLuanModel
+                .Include(b => b.idNguoiDung)
+                .Include(b => b.idPhim)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (binhLuanModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(binhLuanModel);
+        }
+
+        // POST: Admin/BinhLuanModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var binhLuanModel = await _context.BinhLuanModel.FindAsync(id);
+            _context.BinhLuanModel.Remove(binhLuanModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BinhLuanModelExists(int id)
+        {
+            return _context.BinhLuanModel.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ RapChieuPhim/RapChieuPhim/Areas/Admin/Controllers/HoaDonController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RapChieuPhim.Areas.Admin.Data;
+using RapChieuPhim.Areas.Admin.Models;
+
+namespace RapChieuPhim.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HoaDonController : Controller
+    {
+        private readonly DPContext _context;
+
+        public HoaDonController(DPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/HoaDon
+        public async Task<IActionResult> Index()
+        {
+            var dPContext = _context.HoaDonModel.Include(h => h.idNguoiDung);
+            return View(await dPContext.ToListAsync());
+        }
+
+        // GET: Admin/HoaDon/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDonModel = await _context.HoaDonModel
+                .Include(h => h.idNguoiDung)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (hoaDonModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoaDonModel);
+        }
+
+        // GET: Admin/HoaDon/Create
+        public IActionResult Create()
+        {
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID");
+            return View();
+        }
+
+        // POST: Admin/HoaDon/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Tong_tien,Ngay_lap,Da_xoa,NguoiDung_ID")] HoaDonModel hoaDonModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hoaDonModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID", hoaDonModel.NguoiDung_ID);
+            return View(hoaDonModel);
+        }
+
+        // GET: Admin/HoaDon/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDonModel = await _context.HoaDonModel.FindAsync(id);
+            if (hoaDonModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID", hoaDonModel.NguoiDung_ID);
+            return View(hoaDonModel);
+        }
+
+        // POST: Admin/HoaDon/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Tong_tien,Ngay_lap,Da_xoa,NguoiDung_ID")] HoaDonModel hoaDonModel)
+        {
+            if (id != hoaDonModel.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hoaDonModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HoaDonModelExists(hoaDonModel.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDung_ID"] = new SelectList(_context.NguoiDungModel, "ID", "ID", hoaDonModel.NguoiDung_ID);
+            return View(hoaDonModel);
+        }
+
+        // GET: Admin/HoaDon/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDonModel = await _context.HoaDonModel
+                .Include(h => h.idNguoiDung)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (hoaDonModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoaDonModel);
+        }
+
+        // POST: Admin/HoaDon/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hoaDonModel = await _context.HoaDonModel.FindAsync(id);
+            _context.HoaDonModel.Remove(hoaDonModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HoaDonModelExists(int id)
+        {
+            return _context.HoaDonModel.Any(e => e.ID == id);
+        }
+    }
+}
