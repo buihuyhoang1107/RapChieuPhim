e238c416438883e86a5acad51a47a7fa5cd7ad27
kugayamashusei <58841667+kugayamashusei@users.noreply.github.com>
2020-12-22 08:31:07 +0700

    tạo xong modal TaiKhoan

--- RapChieuPhim/RapChieuPhim/wwwroot/js/site.js
+++ RapChieuPhim/RapChieuPhim/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//Them tài khoản
+$(function () {
+    var PlaceHolderElement = $('#PlaceHolderHere');
+    $('button[data-toggle="ajax-modal"]').click(function (event) {
+
+        var url = $(this).data('url');
+        $.get(url).done(function (data) {
+            PlaceHolderElement.html(data);
+            PlaceHolderElement.find('.modal').modal('show');
+        })
+    })
+
+    PlaceHolderElement.on('click', '[data-save="modal"]', function (event) {
+        var form = $(this).parents('.modal').find('form');
+        var actionUrl = form.attr('action');
+        var sendData = form.serialize();
+        $.post(actionUrl, sendData).done(function (data) {
+            PlaceHolderElement.find('.modal').modal('hide');
+        })
+    })
+})
