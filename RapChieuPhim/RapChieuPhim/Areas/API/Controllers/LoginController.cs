57d02deeff0095f4b17aaf3e752bf7c92c9263e4
kugayamashusei <58841667+kugayamashusei@users.noreply.github.com>
2021-01-15 11:27:31 +0700

    tạo xong form đăng nhập admin, và sử lý xong trạng thái đăng nhập

---
+++ RapChieuPhim/RapChieuPhim/Areas/API/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RapChieuPhim.Areas.Admin.Data;
+using RapChieuPhim.Areas.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RapChieuPhim.Areas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DPContext _context;
+        public LoginController(DPContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public bool Post(Login value)
+        {
+            var list = _context.TaiKhoanModel.FirstOrDefault(m => m.Ten_dang_nhap == value.username && m.Mat_khau == value.password && m.Loai_tai_khoan == 1);
+            
+            if(list != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RapChieuPhim/RapChieuPhim/Areas/API/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapChieuPhim.Areas.API.Models
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
